33b7d39db1e600f702f9b2f112ea5651284a82d5
Joey Frohlinger <jmafrohlinger@gmail.com>
2013-01-14 15:32:22 -0500

    created test code to generate a grayscale gradient as an excersize

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,16 +12,22 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QGraphicsScene *scene = new QGraphicsScene(); //create empty scene
     QImage m(QSize(400,400),QImage::Format_RGB32);
-   // m.setPixel(100,100,0);
-    QColor col(0,0,255);
-    cout<<col.rgb()<<endl;
-    m.setPixel(100,100,col.rgb());
+
+    //Creates a gradient in grayscale (this was for experimental purposes)
+    for(int i = 0; i < m.size().width(); i++){
+        for (int j = 0; j< m.size().height(); j++){
+            QColor c((i/(float)m.size().width()*255),(i/(float)m.size().width()*255),(i/(float)m.size().width()*255));
+            m.setPixel(i,j,c.rgb());
+        }
+    }
+
     scene->setSceneRect(m.rect()); //set the scene's view rectangle to the image's
     QPixmap pix = QPixmap::fromImage(m); //create a pixmap from the image
     pix = pix.scaled(ui->renderView->size());
     QGraphicsPixmapItem* pixItem = scene->addPixmap(pix); //add the pixmap to the scene
     ui->renderView->setSceneRect(pix.rect()); //set the renderviews view rectangle
     ui->renderView->setScene(scene); //set the renderViews scene
+    //added comment to check something in git.
 }
 
 MainWindow::~MainWindow()
